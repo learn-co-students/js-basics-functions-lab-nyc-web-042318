562cae0d0e32b70806238fe9af987e1cd71f5790
eanorton <edwardnorton@icloud.com>
2018-06-02 18:10:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks (blocks) {
+  if (blocks > 42) {
+    return blocks - 42;
+  } else {
+    return 42 - blocks;
+  }
+}
+
+function distanceFromHqInFeet (blocks) {
+  return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  if (start > end) {
+    blocksTravelled = start - end;
+    return blocksTravelled * 264;
+  } else {
+    blocksTravelled = end - start;
+    return blocksTravelled * 264;
+  }
+}
+
+function calculatesFarePrice(start, end) {
+  distance = distanceTravelledInFeet(start, end);
+  if (distance < 400) {
+    return 0;
+  } else if (distance > 2500) {
+    return "cannot travel that far";
+  } else if (distance < 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000) {
+    return 25;
+  }
+}
