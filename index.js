7e7ac7af88d72b929bde9df24a2943cef1b6add6
Canino Developments <ian.canino@gmail.com>
2018-06-04 00:58:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(num) {
+    if (num < 42) {
+        return 42 - num
+    } else {
+        return num - 42
+    }
+}
+
+function distanceFromHqInFeet(num) {
+    return result = distanceFromHqInBlocks(num) * 264
+}
+
+function distanceTravelledInFeet(origin, destination) {
+    if ((origin - destination) < 0  ){
+      return (destination - origin) * 264
+    } else if (true) {
+      return (origin - destination) * 264
+
+    }
+}
+
+function calculatesFarePrice(origin, destination) {
+  const ride = distanceTravelledInFeet(origin, destination);
+  if (ride <= 400) {
+    return 0;
+  } else if (ride < 2000) {
+    return ((ride - 400) * 0.02);
+  } else if (ride > 2000 && ride < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
